e632a029023b9452deb8c973a4b2ff2e24cd5d8f
honghayeon <41175122+honghayeon@users.noreply.github.com>
2020-09-04 11:19:23 +0900

    [Update] resgister expression remark

--- app/src/main/java/com/app0/simforpay/global/RegularExpression.kt
+++ app/src/main/java/com/app0/simforpay/global/RegularExpression.kt
@@ -5,12 +5,12 @@ import com.app0.simforpay.R
 
 
 object RegularExpression {
-    val num = "(?=.*[0-9])"
-    val eng = "(?=.*[A-Za-z])"
-    val special = "(?=.*[!@#\\\$%^*+=-])"
-    val pwLength = ".{8,20}"
-    val idLength = ".{6,12}"
-    val phone = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}\$"
+    val num = "(?=.*\\d)" // =(?=.*[0-9])
+    val eng = "(?=.*[a-zA-Z])"
+    val special = "(?=.*\\W)" // =(?=.*[!@#\$%^*+=-])
+    val pwLength = ".{8,20}" // 8~20
+    val idLength = ".{6,12}" // 6~12
+    val phone = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}\$" // 010XXXXXXXX
 
     fun Vaild(editText: EditText) : Boolean {
         var pattern = ""
@@ -18,9 +18,9 @@ object RegularExpression {
         if(editText.id == R.id.phoneNum)
             pattern = phone
         else if(editText.id == R.id.suId)
-            pattern = num + eng
+            pattern = num + eng + idLength
         else if(editText.id == R.id.suPw)
-            pattern = num + eng + special + pwLength // "(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#\$%^*+=-]).{8,20}
+            pattern = num + eng + special + pwLength
 
 //        Log.e("pattern : ", pattern)
 
