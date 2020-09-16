e67dfbc8d02c3d8bd1b8cbb3555d6c07c061fafb
honghayeon <41175122+honghayeon@users.noreply.github.com>
2020-09-17 04:22:49 +0900

    [Change] put the contract share content

--- app/src/main/java/com/app0/simforpay/activity/contract/ContractShareFrag.kt
+++ app/src/main/java/com/app0/simforpay/activity/contract/ContractShareFrag.kt
@@ -1,4 +1,4 @@
-package com.app0.simforpay.activity
+package com.app0.simforpay.activity.contract
 
 import android.graphics.Bitmap
 import android.os.Bundle
@@ -12,21 +12,42 @@ import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.app0.simforpay.R
-import kotlinx.android.synthetic.main.fragment_contract_share.*
+import kotlinx.android.synthetic.main.frag_contract_share.*
 import java.io.File
 import java.io.FileOutputStream
 
+private const val ARG_PARAM1 = "name"
+private const val ARG_PARAM2 = "content"
 
 class ContractShareFrag : Fragment() {
 
     private lateinit var callback: OnBackPressedCallback
+    private var name: String? = null
+    private var content: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            name = it.getString(ARG_PARAM1)
+            content = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contract_share, container, false)
+        return inflater.inflate(R.layout.frag_contract_share, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        title.text = name
+        contractName.text = name
+        contractContent.text = content
     }
 
     override fun onResume() {
@@ -42,6 +63,9 @@ class ContractShareFrag : Fragment() {
         btnSaveImg.setOnClickListener {
             Capture(captureLayout) //지정한 Layout 영역 사진첩 저장 요청
         }
+
+        Log.e("bundle", title.toString())
+        Log.e("bundle", content.toString())
     }
 
     fun Capture(view: View?) {
@@ -75,5 +99,16 @@ class ContractShareFrag : Fragment() {
 
             Toast.makeText(requireContext(), "저장공간 접근 권한이 비활성화되어 있습니다.", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(name: String, content: String) =
+            ContractShareFrag().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, name)
+                    putString(ARG_PARAM2, content)
+                }
+            }
     }
 }
