778563a34de973ef180e5651cd10d8ad989045d8
honghayeon <41175122+honghayeon@users.noreply.github.com>
2020-09-18 01:41:27 +0900

    [Add] bottom sheet

--- app/src/main/java/com/app0/simforpay/activity/home/HomeFrag.kt
+++ app/src/main/java/com/app0/simforpay/activity/home/HomeFrag.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.size
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.app0.simforpay.R
 import com.app0.simforpay.adapter.ContractAdapter
 import com.app0.simforpay.adapter.Data
+import com.app0.simforpay.util.CustomBottomSheetDialog
 import kotlinx.android.synthetic.main.frag_home.*
 
 
@@ -65,7 +67,21 @@ class HomeFrag : Fragment() {
         super.onResume()
 
         btnSearch.setOnClickListener {
-            requireFragmentManager().beginTransaction().add(R.id.layFull, SearchFrag()).commit()
+//            requireFragmentManager().beginTransaction().add(R.id.layFull, SearchFrag()).commit()
+
+            val dialog = CustomBottomSheetDialog.CustomBottomSheetDialogBuilder()
+                .setBtnClickListener(object : CustomBottomSheetDialog.CustomBottomSheetDialogListener {
+                    override fun onClickMenu1Btn() {
+                        Toast.makeText(context, "First Button Clicked", Toast.LENGTH_SHORT).show()
+                    }
+                    override fun onClickMenu2Btn() {
+                        Toast.makeText(context, "Second Button Clicked", Toast.LENGTH_SHORT).show()
+                    }
+                    override fun onClickMenu3Btn() {
+                        Toast.makeText(context, "Third Button Clicked", Toast.LENGTH_SHORT).show()
+                    }
+                }).create()
+            dialog.show(parentFragmentManager, dialog.tag)
         }
 
         btnNotification.setOnClickListener {
