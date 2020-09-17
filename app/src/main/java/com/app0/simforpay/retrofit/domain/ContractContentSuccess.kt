8d42488a38c87062afcf5f745cdd7f59dffbbd1d
Choi-Yeonjin <yeonjin9721@naver.com>
2020-09-17 23:18:03 +0900

    [Update] get data MainViewPager

---
+++ app/src/main/java/com/app0/simforpay/retrofit/domain/ContractContentSuccess.kt
@@ -0,0 +1,7 @@
+package com.app0.simforpay.retrofit.domain
+
+data class ContractContentSuccess (
+    val result: String,
+    val title : String,
+    val content: String
+)
--- app/src/main/java/com/app0/simforpay/retrofit/network/Api.kt
+++ app/src/main/java/com/app0/simforpay/retrofit/network/Api.kt
@@ -2,14 +2,15 @@ package com.app0.simforpay.retrofit.network
 
 import com.app0.simforpay.retrofit.domain.*
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface Api {
 
     @GET("users")
     fun getUsers() : Call<List<User>>
+
+    @GET("contract-contents/{id}")
+    fun getContracts(@Path("id") id: Int): Call<List<ContractContentSuccess>>
 
     @POST("sign-in")
     fun SigninCall(@Body userInfo: Signin) : Call<SigninSuccess>
