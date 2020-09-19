77d714f1cf7a57cbe7f65a216753b0c7041fd712
Choi-Yeonjin <yeonjin9721@naver.com>
2020-09-20 02:06:22 +0900

    [Update] ViewPager bottom sheet Complete And Delete function

--- app/src/main/java/com/app0/simforpay/activity/home/HomeFrag.kt
+++ app/src/main/java/com/app0/simforpay/activity/home/HomeFrag.kt
@@ -29,14 +29,15 @@ class HomeFrag : Fragment() {
     private val Retrofit = RetrofitHelper.getRetrofit()
     private val Title = mutableListOf<String>()
     private val Content = mutableListOf<String>()
-    var cnt=0
+    var cnt = 0
     private var User = arrayOf<String?>()
     private var position: Int? = 0
+    private var getContractContent = listOf<ContractContentSuccess>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var id=Integer.parseInt(MyApplication.prefs.getString(Key.LENDER_ID.toString(), ""))
+        var id = Integer.parseInt(MyApplication.prefs.getString(Key.LENDER_ID.toString(), ""))
 
         Retrofit.getUser(id).enqueue(object : Callback<User> {
             override fun onResponse(call: Call<User>, response: Response<User>) {
@@ -57,7 +58,7 @@ class HomeFrag : Fragment() {
             position = it.getInt(ARG_PARAM3)
         }
 
-        Log.d("posi",position.toString())
+        Log.d("posi", position.toString())
 
     }
 
@@ -72,10 +73,13 @@ class HomeFrag : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var id=Integer.parseInt(MyApplication.prefs.getString(Key.LENDER_ID.toString(), ""))
+
+
+        var id = Integer.parseInt(MyApplication.prefs.getString(Key.LENDER_ID.toString(), ""))
 
         Retrofit.getContracts(id).enqueue(object : Callback<List<ContractContentSuccess>> {
-            override fun onResponse(call: Call<List<ContractContentSuccess>>, response: Response<List<ContractContentSuccess>>) {
+            override fun onResponse( call: Call<List<ContractContentSuccess>>, response: Response<List<ContractContentSuccess>> ) {
+                getContractContent = response.body()!!
                 response.body()?.forEach {
                     Title.add(it.title)
                     Content.add(it.content)
@@ -84,15 +88,12 @@ class HomeFrag : Fragment() {
 
                 val list = ArrayList<Data>()
 
-                for(i in 0 until cnt){
+                for (i in 0 until cnt) {
                     val item = Data(Title[i], Content[i])
                     list += item
                 }
-
-                if(position != null) {
-                    vpContract.adapter = ContractAdapter(list, requireContext(), parentFragmentManager)
-                    vpContract.setCurrentItem(position!!)
-                }
+                vpContract.adapter = ContractAdapter(list, requireContext(), parentFragmentManager, getContractContent)
+                vpContract.setCurrentItem(position!!)
             }
 
             override fun onFailure(call: Call<List<ContractContentSuccess>>, t: Throwable) {}
@@ -103,24 +104,33 @@ class HomeFrag : Fragment() {
         val dpValue = 80
         val displaySize = resources.displayMetrics.density
         val margin = (dpValue * displaySize).toInt()
-        vpContract.setPadding(margin/2, 0, margin+margin/2, 0)
-        vpContract.pageMargin = margin/2
+        vpContract.setPadding(margin / 2, 0, margin + margin / 2, 0)
+        vpContract.pageMargin = margin / 2
 
         vpContract.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
 
             }
 
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
 
             }
 
             override fun onPageSelected(position: Int) {
                 // 첫페이지와 끝페이지 padding, margin 값 조절
-                Log.d("page",position.toString())
+                Log.d("page", position.toString())
                 when (position) {
-                    0 -> vpContract.setPadding(margin/2, 0, margin+margin/2, 0)
-                    vpContract.size-1 -> vpContract.setPadding(margin+margin/2, 0, margin/2, 0)
+                    0 -> vpContract.setPadding(margin / 2, 0, margin + margin / 2, 0)
+                    vpContract.size - 1 -> vpContract.setPadding(
+                        margin + margin / 2,
+                        0,
+                        margin / 2,
+                        0
+                    )
                     else -> vpContract.setPadding(margin, 0, margin, 0)
                 }
 
@@ -133,12 +143,16 @@ class HomeFrag : Fragment() {
 
         btnSearch.setOnClickListener {
             val list = ArrayList<Data>()
-            vpContract.adapter = ContractAdapter(list, requireContext())
-            requireFragmentManager().beginTransaction().replace(R.id.layFull, SearchFrag()).addToBackStack(null).commit()
+            vpContract.adapter = ContractAdapter(list, requireContext(), parentFragmentManager, getContractContent)
+            requireFragmentManager().beginTransaction().replace(R.id.layFull, SearchFrag())
+                .addToBackStack(null).commit()
         }
 
         btnNotification.setOnClickListener {
-            requireFragmentManager().beginTransaction().replace(R.id.layFull, NotificationFrag()).addToBackStack(null).commit()
+            val list = ArrayList<Data>()
+            vpContract.adapter = ContractAdapter(list, requireContext(), parentFragmentManager, getContractContent)
+            requireFragmentManager().beginTransaction().replace(R.id.layFull, NotificationFrag())
+                .addToBackStack(null).commit()
         }
 
         btnMypage.setOnClickListener {
@@ -147,10 +161,25 @@ class HomeFrag : Fragment() {
 //            }
 
             val list = ArrayList<Data>()
-            vpContract.adapter = ContractAdapter(list, requireContext())
-            requireFragmentManager().beginTransaction().replace(R.id.layFull,
-                MypageFrag.newInstance(User[0]!!, User[1]!!, User[2]!!, User[3]!!, User[4]!!, User[5]!!)
+            vpContract.adapter = ContractAdapter(list, requireContext(), parentFragmentManager, getContractContent)
+            requireFragmentManager().beginTransaction().replace(
+                R.id.layFull,
+                MypageFrag.newInstance(
+                    User[0]!!,
+                    User[1]!!,
+                    User[2]!!,
+                    User[3]!!,
+                    User[4]!!,
+                    User[5]!!
+                )
             ).addToBackStack(null).commit()
+        }
+
+        btnRefresh.setOnClickListener{
+            val list = ArrayList<Data>()
+            vpContract.adapter = ContractAdapter(list, requireContext(), parentFragmentManager, getContractContent)
+            requireFragmentManager().beginTransaction().replace(R.id.layFull, HomeFrag())
+                .addToBackStack(null).commit()
         }
     }
 
--- app/src/main/java/com/app0/simforpay/retrofit/domain/ContractContentSuccess.kt
+++ app/src/main/java/com/app0/simforpay/retrofit/domain/ContractContentSuccess.kt
@@ -1,7 +1,17 @@
 package com.app0.simforpay.retrofit.domain
 
 data class ContractContentSuccess (
-    val result: String,
+    val id : Int,
     val title : String,
-    val content: String
+    val borrowDate : String,
+    val paybackDate : String,
+    val price : Int,
+    val lenderName : String,
+    val lenderBank : String,
+    val lenderAccount : Int,
+    val borrower : List<BorrowerSuccess>,
+    val penalty : String,
+    val content : String,
+    val alarm : Int,
+    val state : Int
 )
