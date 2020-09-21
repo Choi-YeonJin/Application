69ffa834ad8a2f5488ef5bed34d862b27595e6fb
honghayeon <41175122+honghayeon@users.noreply.github.com>
2020-09-21 17:46:29 +0900

    [Change] package

--- app/src/main/java/com/app0/simforpay/activity/setting/SettingFrag.kt
+++ app/src/main/java/com/app0/simforpay/activity/setting/SettingFrag.kt
@@ -1,4 +1,4 @@
-package com.app0.simforpay.activity
+package com.app0.simforpay.activity.setting
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,20 +14,20 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CalendarFrag.newInstance] factory method to
+ * Use the [SettingFrag.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CalendarFrag : Fragment() {
+class SettingFrag : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+            super.onCreate(savedInstanceState)
+            arguments?.let {
+                param1 = it.getString(ARG_PARAM1)
+                param2 = it.getString(ARG_PARAM2)
+            }
     }
 
     override fun onCreateView(
@@ -35,7 +35,7 @@ class CalendarFrag : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frag_calendar, container, false)
+        return inflater.inflate(R.layout.frag_setting, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class CalendarFrag : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CalendarFrag.
+         * @return A new instance of fragment SettingFrag.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CalendarFrag().apply {
+            SettingFrag().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
